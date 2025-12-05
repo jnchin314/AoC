ac82fe431474450d3f5e8bee786b2ad00a4115b9
james <jnchin.james@gmail.com>
2025-12-04 21:59:15 -0500

    AoC2025 - day4 part1 and part 2 (Sorry not naming variables more concisely)

---
+++ src/main/java/org/adventofcode/y2025/day4/Day4Part1.java
@@ -0,0 +1,56 @@
+package org.adventofcode.y2025.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.adventofcode.y2025.ReadFile;
+import org.adventofcode.y2025.day3.Day3Part1;
+
+public class Day4Part1 extends ReadFile {
+    int count = 0;
+    List<String> rollMap = new ArrayList<>();
+    public Day4Part1() {
+        super("src/main/resources/y2025/day4/input.txt");
+        int count = 0;
+        int lineLength = lines.getFirst().length();
+        StringBuilder sb = new StringBuilder();
+        sb.repeat(".", lineLength+2);
+        rollMap.add(sb.toString());
+        for (String line : lines) {
+            sb = new StringBuilder();
+            rollMap.add(sb.append(".").append(line).append(".").toString());
+        }
+        sb = new StringBuilder();
+        sb.repeat(".", lineLength+2);
+        rollMap.add(sb.toString());
+        rollMap.forEach(System.out::println);
+        for(int i = 1;i < lineLength + 1;i++){
+            for(int j = 1;j < rollMap.size() -1;j++){
+                if(checkRoll(i,j)){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+
+
+    public boolean checkRoll(int row, int col){
+        boolean isCheckableSpot = rollMap.get(row).charAt(col) == '@';
+        int neighborRolls = 0;
+        for(int i = -1; i <= 1;i++){
+            for (int j = -1;j <= 1;j++){
+                if(!(i == 0 && j == 0) && isCheckableSpot && rollMap.get(row + i).charAt(col+j) == '@'){
+                    neighborRolls++;
+                }
+            }
+        }
+        System.out.println("row : " + row + " col : " + col + " spot : " + rollMap.get(row).charAt(col) + " : count " + neighborRolls);
+        return neighborRolls < 4 && isCheckableSpot;
+    }
+
+    public static void main(String[] args) {
+        Day4Part1 day4Part1 = new Day4Part1();
+    }
+}
---
+++ src/main/java/org/adventofcode/y2025/day4/Day4Part2.java
@@ -0,0 +1,69 @@
+package org.adventofcode.y2025.day4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.adventofcode.y2025.ReadFile;
+
+public class Day4Part2 extends ReadFile {
+    int count = 0;
+    int lineLength;
+    List<String> rollMap = new ArrayList<>();
+    public Day4Part2() {
+        super("src/main/resources/y2025/day4/input.txt");
+        lineLength = lines.getFirst().length();
+        StringBuilder sb = new StringBuilder();
+        sb.repeat(".", lineLength+2);
+        rollMap.add(sb.toString());
+        for (String line : lines) {
+            sb = new StringBuilder();
+            rollMap.add(sb.append(".").append(line).append(".").toString());
+        }
+        sb = new StringBuilder();
+        sb.repeat(".", lineLength+2);
+        rollMap.add(sb.toString());
+        rollMap.forEach(System.out::println);
+        int iterationCount = processMap();
+        count += iterationCount;
+        while(iterationCount > 0){
+            iterationCount = processMap();
+            count += iterationCount;
+        }
+        System.out.println("total count " + count);
+    }
+
+    public int processMap(){
+        int count = 0;
+        rollMap.forEach(System.out::println);
+        for(int i = 1;i < lineLength + 1;i++){
+            for(int j = 1;j < rollMap.size() -1;j++){
+                if(checkRoll(i,j)){
+                    count++;
+                    StringBuilder sb = new StringBuilder(rollMap.get(i));
+                    sb.replace(j,j+1, ".");
+                    rollMap.set(i, sb.toString());
+                }
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+
+    public boolean checkRoll(int row, int col){
+        boolean isCheckableSpot = rollMap.get(row).charAt(col) == '@';
+        int neighborRolls = 0;
+        for(int i = -1; i <= 1;i++){
+            for (int j = -1;j <= 1;j++){
+                if(!(i == 0 && j == 0) && isCheckableSpot && rollMap.get(row + i).charAt(col+j) == '@'){
+                    neighborRolls++;
+                }
+            }
+        }
+        //System.out.println("row : " + row + " col : " + col + " spot : " + rollMap.get(row).charAt(col) + " : count " + neighborRolls);
+        return neighborRolls < 4 && isCheckableSpot;
+    }
+
+    public static void main(String[] args) {
+        Day4Part2 day4Part1 = new Day4Part2();
+    }
+}
